73368dee89e16f72f8480c80b07167df90ccc2f5
cafesun <cafesun_global@163.com>
2017-09-10 19:37:24 +0800

    添加异常链样例代码处理

--- ThinkJava/src/org/think/ex/ExTemplateTwoTuple.java
+++ ThinkJava/src/org/think/ex/ExTemplateTwoTuple.java
@@ -1,10 +1,10 @@
 package org.think.ex;
 
-public class ExTwoTuple<T,S> {
+public class ExTemplateTwoTuple<T,S> {
     public final T first;
     public final S second;
 
-    public ExTwoTuple(T a, S b) {
+    public ExTemplateTwoTuple(T a, S b) {
         first = a;
         second = b;
     }
@@ -15,8 +15,8 @@ public class ExTwoTuple<T,S> {
     }
 
     public static void main(String [] args) {
-        ExTwoTuple<String, String> oTupleStr = new ExTwoTuple<String, String>("abc", "123");
-        ExTwoTuple<Integer, Integer> oTupleint = new ExTwoTuple<Integer, Integer>(1, 100);
+        ExTemplateTwoTuple<String, String> oTupleStr = new ExTemplateTwoTuple<String, String>("abc", "123");
+        ExTemplateTwoTuple<Integer, Integer> oTupleint = new ExTemplateTwoTuple<Integer, Integer>(1, 100);
         System.out.println(oTupleStr);
         System.out.println(oTupleint);
     }
