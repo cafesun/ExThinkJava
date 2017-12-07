a0bfe975f2c6deba3e3a1a96e9c5c7279c9a447f
cafesun <cafesun_cloud@163.com>
2017-12-07 12:11:59 +0800

    commit

---
+++ ThinkJava/src/org/think/ex/ExStringSample.java
@@ -0,0 +1,9 @@
+package org.think.ex;
+
+public class ExStringSample {
+
+    public static void main(String[] args) {
+        String strMessage = "Heelo";
+        System.out.println(strMessage);
+    }
+}
